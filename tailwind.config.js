6e2af9c0157a65d719447ca6c83310a33ec88b52
Ian Main <imitchell.main@gmail.com>
2022-06-09 11:37:26 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
       backgroundImage: {
         'cat-pattern' : 'url("https://www.fundacion-affinity.org/sites/default/files/los-10-sonidos-principales-del-gato-fa.jpg")',
         'paw-pattern' : 'url("https://i.imgur.com/5WotzPu.png")',
+        'cat' : 'url("https://i.imgur.com/BntdGr7.png")',
       }
     },
   },
